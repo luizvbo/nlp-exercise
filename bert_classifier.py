ef362e93d3aac6d08ea8255228c375d175904ac6
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2020-04-14 18:14:40 +0200

    Use lookup dictionary

--- bert_classifier.py
+++ bert_classifier.py
@@ -16,21 +16,22 @@
 import tensorflow as tf
 from transformers import (
     TFDistilBertForSequenceClassification,
-    DistilBertTokenizer,
 )
+from transformers.tokenization_distilbert import DistilBertTokenizerFast
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.model_selection import train_test_split
 
 from utils import MAX_LEN
-from tqdm import tqdm
-import numpy as np
 
+import os
+import json
 import spacy
 
 spacy_nlp = spacy.load('en_core_web_sm')
 
-BERTMODEL = "distilbert-base-uncased"
+BERTMODEL = "distilbert-base-cased"
+PATH_CACHED_OUTPUT = '.cache/distilbert_outputs.json'
 BATCH_SIZE = 32
 N_EPOCHS = 30
 LEARNING_RATE = 3e-5
@@ -46,111 +47,101 @@ class BertClassifier(BaseEstimator, ClassifierMixin):
         self.n_epochs = n_epochs
         self.val_size = val_size
         self.learning_rate = learning_rate
+        self.cached_output = None
 
         # Load dataset, tokenizer, model from pretrained model/vocabulary
         self.tokenizer = (
-            DistilBertTokenizer
-            .from_pretrained(BERTMODEL, cache_dir='./.cache/',
-                             do_lower_case=True)
+            DistilBertTokenizerFast
+            .from_pretrained(BERTMODEL, do_lower_case=False)
         )
         self.model = (
             TFDistilBertForSequenceClassification
-            .from_pretrained(BERTMODEL, cache_dir='./.cache/')
+            .from_pretrained(BERTMODEL)
         )
 
-        # Freezes all the layers (except the classifier and dropout)
-        print("Trainable layers")
-        print("=" * 16)
-        for layer in self.model.layers:
-            frozen_layers = ('distilbert')
-            layer.trainable = layer.name not in frozen_layers
-            print(layer.name + ":",  layer.trainable)
+        # Overwrite the 'distilbert' layer
+        self.model.distilbert = self._distilbert_wrapper(self.model.distilbert)
+
+    def _distilbert_wrapper(self, distilbert):
+
+        if self.cached_output is None:
+            if os.path.isfile(PATH_CACHED_OUTPUT):
+                with open(PATH_CACHED_OUTPUT) as f:
+                    self.cached_output = json.load(f)
+            else:
+                self.cached_output = {}
+
+        def _distilbert(inputs, **kwargs):
+            t_inputs = tuple(inputs)
+            if t_inputs in self.cached_output:
+                distilbert_output = self.cached_output[t_inputs].values
+            else:
+                distilbert_output = self.model.distilbert(inputs, **kwargs)
+                self.cached_output[t_inputs] = distilbert_output
+
+            return distilbert_output
+        return _distilbert
 
     def summary(self):
         # Print model summary
         self.model.summary()
 
     def tokenize_sentences(self, sentences):
-        input_ids = []
-        attention_masks = []
-
-        for sentence in tqdm(sentences):
-            # `encode_plus` will:
-            #   (1) Tokenize the sentence.
-            #   (2) Prepend the `[CLS]` token to the start.
-            #   (3) Append the `[SEP]` token to the end.
-            #   (4) Map tokens to their IDs.
-            #   (5) Pad or truncate the sentence to `max_length`
-            #   (6) Create attention masks for [PAD] tokens.
-            encoded_dict = (
-                self.tokenizer.encode_plus(
-                    sentence,                     # Sentence to encode.
-                    add_special_tokens=True,      # Add '[CLS]' and '[SEP]'
-                    max_length=self.max_seq_len,  # Pad & truncate.
-                    pad_to_max_length=True,
-                    return_attention_mask=True,   # Construct attn. masks.
-                )
+        # `encode_plus` will:
+        #   (1) Tokenize the sentence.
+        #   (2) Prepend the `[CLS]` token to the start.
+        #   (3) Append the `[SEP]` token to the end.
+        #   (4) Map tokens to their IDs.
+        #   (5) Pad or truncate the sentence to `max_length`
+        #   (6) Create attention masks for [PAD] tokens.
+        encoded_dict = (
+            self.tokenizer.batch_encode_plus(
+                sentences,                   # Sentence to encode.
+                add_special_tokens=True,     # Add '[CLS]' and '[SEP]'
+                max_length=MAX_LEN,          # Pad & truncate all sentences.
+                pad_to_max_length=True,
+                return_attention_mask=True,  # Construct attn. masks.
+                return_tensors='tf',
             )
-
-            # Add the encoded sentence to the list.
-            input_ids.append(encoded_dict['input_ids'])
-            # And its attention mask (simply differentiates padding
-            # from non-padding).
-            attention_masks.append(encoded_dict['attention_mask'])
-
-        return (
-            np.array(input_ids, dtype=np.int32),
-            np.array(attention_masks, dtype=np.int32)
         )
 
-    def create_dataset(self, X, y, train=True):
+        return encoded_dict['input_ids'], encoded_dict['attention_mask']
 
-        input_ids, att_masks = self.tokenize_sentences(X)
-        # Convert labels to int32
-        labels = y
-
-        def gen():
-            for in_id, att_mk, label in zip(input_ids, att_masks, labels):
-                yield ({"input_ids": in_id, "attention_mask": att_mk}, label,)
-
-        return tf.data.Dataset.from_generator(
-            gen, ({"input_ids": tf.int32,
-                   "attention_mask": tf.int32}, tf.bool),
-            ({"input_ids": tf.TensorShape([None]),
-              "attention_mask": tf.TensorShape([None])}, tf.TensorShape([])),
-        )
+    # set up the epoches to have better accuracy
+    def train(self, X_train, y_train, X_val, y_val):
+        self.scores = []
+        self.loss = []
+        num_batch = int(X_train.shape[0] / self.batch_size)
+        for i in range(num_batch-1):
+            start = i * self.batch_size
+            end = (i + 1) * self.batch_size
+            history = self.model.fit(X_train[start:end], y_train[start:end],
+                                     validation_data=(X_val, y_val),
+                                     epochs=self.n_epochs)
+            self.scores.append(history.history['val_accuracy'])
+            self.loss.append(history.history['val_loss'])
 
     def fit(self, X, y):
         X_train, X_val, y_train, y_val = (
             train_test_split(X, y, test_size=self.val_size)
         )
 
-        print("Creating training set")
-        ds_train = self.create_dataset(X_train, y_train)
-        print("Creating validation set")
-        ds_val = self.create_dataset(X_val, y_val)
+        ds_train = tf.data.Dataset.from_tensors(
+            (self.tokenize_sentences(X_train.tolist()), y_train)
+        ).shuffle(buffer_size=1024).batch(self.batch_size)
 
-        # %% colab={} colab_type="code" id="a-JK58wy8tPn"
-        ds_train = (
-            ds_train.shuffle(1000)
-            .batch(self.batch_size)
-            .repeat(self.n_epochs)
-        )
-        ds_val = ds_val.batch(self.batch_size)
+        ds_val = tf.data.Dataset.from_tensors(
+            (self.tokenize_sentences(X_val.tolist()), y_val)
+        ).batch(64)
 
-        steps_per_epoch = X_train.shape[0] // self.batch_size
-        validation_steps = X_val.shape[0] // self.batch_size
-
-        # Prepare training: Compile tf.keras model with optimizer, loss and
-        # learning rate schedule
-        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
-
-        self.model.compile(optimizer=optimizer,
-                           loss='binary_crossentropy',
+        self.model.compile(optimizer='adam', loss='binary_crossentropy',
                            metrics=['accuracy'])
+        # self.train(X_train, y_train, X_val, y_val)
 
-        self.history = self.model.fit(ds_train, epochs=self.n_epochs,
-                                      steps_per_epoch=steps_per_epoch,
-                                      validation_data=ds_val,
-                                      validation_steps=validation_steps)
+        self.model.fit(ds_train, epochs=self.n_epochs,
+                       steps_per_epoch=115,
+                       validation_data=ds_val,
+                       validation_steps=7)
 
+        with open(PATH_CACHED_OUTPUT, 'w') as f:
+            json.dump(self.cached_output, f)
