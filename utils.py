821d42d9095d9c523f4ffe112b8ce467acccad28
Olle Dahlen <olle.dahlen@mendesgans.nl>
2020-04-14 15:30:59 +0200

    updated to test set accuracy and save bert features

--- utils.py
+++ utils.py
@@ -28,9 +28,11 @@ def get_x_y(df):
 def results(df, make_model, n_data_points, batch_size_inference=100):
     X, y = get_x_y(df)
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
     for n_data_point in n_data_points:
         _, X_sample, _, y_sample = train_test_split(
-            X, y, test_size=n_data_point - 1, random_state=0)
+            X_train, y_train, test_size=n_data_point - 1, random_state=0)
 
         model = make_model()
         print(f'Fits for number of data points:{n_data_point}')
@@ -39,16 +41,16 @@ def results(df, make_model, n_data_points, batch_size_inference=100):
         print('Predicts and computes accuracy for the entire data set')
         
         # Performs inference batched due to memory error
-        predictions = []
+        test_predictions = []
         for batch_end_index in range(batch_size_inference, 
                                      len(y) + batch_size_inference, 
                                      batch_size_inference):
             
-            batch = X[batch_end_index - batch_size_inference:batch_end_index]
+            batch = X_test[batch_end_index - batch_size_inference:batch_end_index]
             batch_predictions = model.predict(batch)
-            predictions.extend(list(batch_predictions))
+            test_predictions.extend(list(batch_predictions))
         
-        a = accuracy_score(y, predictions)
+        a = accuracy_score(y_test, test_predictions)
 
         yield {
             'n_data_points': n_data_point,
